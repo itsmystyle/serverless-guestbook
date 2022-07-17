50fb220cdfe3d7082e77c527003d4e90d5857d0d
Ywuan-Chai Chong <r07922141@ntu.edu.tw>
2022-07-17 18:42:49 +0800

    add api url

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://0de01dee.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
